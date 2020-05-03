5d1ef904ea09886de0b8bd3da703b42b204d77bb
oyinkan <skusman@student.lautech.edu.ng>
2020-05-03 20:31:07 +0100

    Updates

---
+++ precache-manifest.1e27a7a22a2f369ce8981f374a38f9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f52aba695e9fca7f9a504b0b023a75",
+    "url": "/pricing-plans/index.html"
+  },
+  {
+    "revision": "1f8d6c7f367ce13189cb",
+    "url": "/pricing-plans/static/css/main.fa6360c4.chunk.css"
+  },
+  {
+    "revision": "fa88a058ccc7ab320427",
+    "url": "/pricing-plans/static/js/2.4e4711e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pricing-plans/static/js/2.4e4711e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8d6c7f367ce13189cb",
+    "url": "/pricing-plans/static/js/main.74afde83.chunk.js"
+  },
+  {
+    "revision": "4ec83a2d8d98cd11a7b5",
+    "url": "/pricing-plans/static/js/runtime-main.6e973b91.js"
+  }
+]);
